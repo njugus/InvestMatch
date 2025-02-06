6cc67fe12fcbb454b7dc3227ff446b2095acc09b
njugus <kayvohnjuguna@gmail.com>
2025-02-06 10:52:33 -0800

    sets up the authorization middleware to check whether users have a token or not

---
+++ Server/Services/Controllers/Investor.controllers.js
@@ -0,0 +1,16 @@
+import Investor from '../Models/Investor.model.js'
+
+//create an investor's profile
+ 
+const investorProfile = async(req, res) => {
+    try{
+        //destructure the details from the req.body
+        const { Name, Mission, Vision, Preferences } = req.body
+
+        //get access to the token to generate UserID
+        const 
+
+    }catch(error){
+        //
+    }
+}
