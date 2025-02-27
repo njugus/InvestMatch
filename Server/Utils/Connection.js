7d57ed05620252bd7d8bb9ff29f6732e6c1757ca
njugus <kayvohnjuguna@gmail.com>
2025-02-26 20:18:11 -0800

    adds some changes to the utils method

--- Server/Utils/Connection.js
+++ Server/Utils/Connection.js
@@ -1,29 +1,3 @@
-// import { MongoClient } from "mongodb";
-// import dotenv from "dotenv";
-
-// dotenv.config(); // Load environment variables from .env file
-
-// const uri = process.env.MONGO_URI;
-
-// const connectDB = async () => {
-//     try {
-//         const client = new MongoClient(uri, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-
-//         await client.connect();
-//         console.log(`MongoDB Connected: ${client.s.options.srvHost}`);
-
-//         return client.db("InvestMatch"); // Return the connected database instance
-//     } catch (error) {
-//         console.log(`Error: ${error.message}`);
-//         process.exit(1); // Exit the process if connection fails
-//     }
-// };
-
-// export { connectDB, client}
-
 
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
@@ -42,7 +16,7 @@ const connectDB = async () => {
         await client.connect();
         console.log(`MongoDB Connected: ${client.s.options.srvHost}`);
         return client.db("InvestMatch"); // Explicitly return the correct database
-        
+
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
